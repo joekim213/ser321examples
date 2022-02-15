f62308d5fc522546f0f73ac39ba1207ef930e271
joekim213 <j.soohwankim@gmail.com>
2022-02-15 13:11:54 -0800

    testing

--- Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
+++ Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
@@ -255,7 +255,7 @@ class WebServer {
 
           String parseJson = json;
 
-          Pattern p = Pattern.compile("(\\w+)\\.(\\w+)");
+          Pattern p = Pattern.compile("([id]\\w+)\\:([id]\\w+)");
 
           Matcher matcher = p.matcher(parseJson);
 
